7341f8f7effb150f4265e80ad255fc17b82db706
Digambaran <arjunsreekumar.1@gmail.com>
2022-07-22 19:28:12 +0530

    red button

--- src/todoInputRed.js
+++ src/todoInputRed.js
@@ -1,25 +1,25 @@
-import React from 'react'
-import env from 'env'
+import React from "react";
+import env from "env";
 export const TodoInputRed = ({ refetch }) => {
-  const [todo, setTodo] = React.useState('')
-  const [submitting, setSubmitting] = React.useState(false)
+  const [todo, setTodo] = React.useState("");
+  const [submitting, setSubmitting] = React.useState(false);
   const handleAdd = async () => {
-    setSubmitting(true)
+    setSubmitting(true);
     try {
       const res = await fetch(`${env.BLOCK_FUNCTION_URL}/addTodo`, {
-        method: 'post',
+        method: "post",
         body: JSON.stringify({ name: todo }),
-      })
+      });
 
-      const resp = await res.json()
-      console.log(resp)
-      setSubmitting(false)
-      refetch(true)
+      const resp = await res.json();
+      console.log(resp);
+      setSubmitting(false);
+      refetch(true);
     } catch (e) {
-      console.log(e)
-      setSubmitting(false)
+      console.log(e);
+      setSubmitting(false);
     }
-  }
+  };
   return (
     <>
       <div className="flex center gap-x-4 my-5 mt-8">
@@ -36,14 +36,14 @@ export const TodoInputRed = ({ refetch }) => {
             type="button"
             disabled={submitting}
             onClick={handleAdd}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline whitespace-nowrap"
+            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline whitespace-nowrap"
           >
             Add
           </button>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default TodoInputRed
+export default TodoInputRed;
